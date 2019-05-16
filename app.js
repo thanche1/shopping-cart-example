5377fe12b5a5a3b553d0e99de88c410cddf117c1
Thanche <thanche91@gmail.com>
2019-05-16 17:30:16 +0300

    second commit

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 //variables
 
-const cartBtn = document.querySelector( ".cart-btn" );
-const closeCartBtn = document.querySelector( ".close-cart" );
-const clearCart = document.querySelector( ".clear-cart" );
-const cartDOM = document.querySelector( ".cart" );
-const cartOverlay = document.querySelector( ".cart-overlay" );
-const cartItems = document.querySelector( ".cart-items" );
-const cartTotal = document.querySelector( ".cart-total" );
-const cartContent = document.querySelector( ".cart-content" );
-const productsDOM = document.querySelector( ".products-center" );
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCart = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// const btns = document.querySelectorAll(".bag-btn")
 
 // cart 
 let cart = [];
 
+//buttons
+let buttonsDOM = [];
+
 //Getting the products 
 class Products {
 
-   async getProducts() {
-       try{
-        let result = await fetch('products.json')
-        let data = await result.json();
+    async getProducts() {
+        try {
+            let result = await fetch('products.json')
+            let data = await result.json();
 
-        let products = data.items;
-        products = products.map( item =>{
-            const {title,price}=item.fields
-            const {id} = item.sys
-            const image = item.fields.image.fields.file.url;
-            return {title,price,id,image}
+            let products = data.items;
+            products = products.map(item => {
+                const { title, price } = item.fields
+                const { id } = item.sys
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image }
 
-             
-        })
-        return products
-        
 
-       } catch (error) {
-           console.log( error )
-       }
+            })
+            return products
 
-     
+
+        } catch (error) {
+            console.log(error)
+        }
+
+
     }
 
 }
 
-//UI class for getting items and displaing them .
+//UI class for getting items and displaying them .
 class UI {
-    displayProducts( products ) {
-     let result = '';
-     products.forEach(product => {
-         result +=`
+    displayProducts(products) {
+        let result = '';
+        products.forEach(product => {
+            result += `
           <article class="product">
          <div class="img-container">
              <img
@@ -63,19 +68,77 @@ class UI {
      </article>
          
          `;
-     });
-     productsDOM.innerHTML = result;
+        });
+        productsDOM.innerHTML = result;
 
     }
+    getBagButtons() {
+        const buttons = [...document.querySelectorAll(".bag-btn")];
+        buttonsDOM = buttons;
+        buttons.forEach(button => {
+            let id = button.dataset.id;
+            let inCart = cart.find(item => item.id === id);
+            if (inCart) {
+                button.innerText = "In Cart";
+                button.disabled = true
+            }
+                button.addEventListener('click', (event) => {
+                   event.target.innerText = "In Cart";
+                   event.target.disabled = true;
+                    
+                   //get product from products
+
+                   let cartItem = {...Storage.getProduct( id ),amount:1};
+                   
+                   cart = [...cart,cartItem];
+                   Storage.saveCart( cart );
+                   
+                   
+                   //Add porduct to the cart
+                   //Save the cart in the local storage.
+                   //set cart values
+                   //Display cart item.
+                   // Show the cart
+                });
+            
+
+        });
+
+}
 }
 //local storage
-class Storage {}
+class Storage {
+    static saveProducts(products) {
+        localStorage.setItem("products", JSON.stringify(products));
+    }
 
-document.addEventListener( "DOMContentLoaded", ()=>{
+    
+                   //get product from products
+    static getProduct( id ) {
+        let products = JSON.parse( localStorage.getItem( 'products'));
+        return products.find( product => product.id === id )
+    }
+
+    static saveCart( cart ) {
+        localStorage.setItem( 'cart', JSON.stringify( cart ));
+
+    }
+
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
 
     //get all products
 
-    products.getProducts().then( products =>ui.displayProducts(products));
-})
+    products.getProducts().then(products => {
+        ui.displayProducts(products);
+        Storage.saveProducts(products);
+    }).then(() => {
+        ui.getBagButtons();
+
+
+    });
+});
